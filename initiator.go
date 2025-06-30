ec6f79127d765a2f20bc87da3661f8620bac132e
AlexandrosKyriakakis <alexandros.kyriakakis@alpaca.markets>
2025-07-01 00:54:41 +0300

    minor

--- initiator.go
+++ initiator.go
@@ -238,7 +238,7 @@ func (i *Initiator) getInChanCapacity(sessionID SessionID) int {
 
 	inChanCapacityVal, err := strconv.Atoi(inChanCapacityStr)
 	if err != nil || inChanCapacityVal < 0 {
-		i.sessions[sessionID].log.OnEventf("Invalid value for setting %s, must be a non-negative integer, will default to 0: %v", initiatorInChanCapacityConfig, err)
+		i.sessions[sessionID].log.OnEventf("Invalid value for setting %s, must be a non-negative integer, will default to 0 (unbuffered): %v", initiatorInChanCapacityConfig, err)
 		return 0
 	}
 
